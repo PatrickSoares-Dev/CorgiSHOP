b597a196de195e4604ec11e1a8eef75db517ccf7
Patrick Oliveira <patrick.oliveira@telecall.com>
2023-06-13 14:06:21 -0300

    port 80 viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/CorgiSHOP",
-  plugins: [react()],
+  base: '/CorgiSHOP',
+  server: {
+    host: true,
+    port: 80,
+  },
+  plugins: [react()]
 })
